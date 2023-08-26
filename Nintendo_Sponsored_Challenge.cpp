1dd9318af86e74b6e6ec5e588d6cbccc569abfcc
Garip KUSOGLU <garip.doga.kusoglu@gmail.com>
2023-08-26 08:21:36 +0200

    A very hard problem !

--- Nintendo_Sponsored_Challenge.cpp
+++ Nintendo_Sponsored_Challenge.cpp
@@ -14,97 +14,111 @@ typedef unsigned short u16;
 typedef unsigned int u32;
 typedef uint64_t u64;
 typedef unsigned __int128 u128;
+#define bitset(byte,nbit)   ((byte) |=  (1<<(nbit)))
+#define bitclear(byte,nbit) ((byte) &= ~(1<<(nbit)))
+#define bitflip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
+#define bitcheck(byte,nbit) ((byte) &   (1<<(nbit)))
 
-stringstream s0;
-u32* decode(u32 *b, u32 size)
-{
-
-    u32* a = new u32[size / 16];
-
-    for(u32 i = 0; i < size / 16; ++i)
-        a[i] = 0;//xffffffff;
-    u32* b_prev = new u32[size / 16];
-    
-    for (u32 i = 0; i < size / 16; i++)
-        b_prev[i] = b[i];
-
-    // for (int i = size-1; i >= 0; i--)
-    //     for (int j = size-1; j >= 0; j--)
-    for (u32 i = 0; i < size; i++)
-        for (u32 j = 0; j < size; j++)
-        {
-            if((b_prev[(i + j) / 32] >> ((i+j) % 32)) & 1)
-            {
-                a[i / 32] |= 1 << (i % 32);
-                a[j / 32 + size / 32] |= 1 << (j % 32);
-                b_prev[(i + j) / 32] ^= 1 << ((i+j) % 32);
-            }
-        }
-    return a;
-}
+#define bitsetclear(byte,nbit,bit)   ((bit) == 0 ? (bitclear((byte),(nbit))) : (bitset(byte,nbit)))
+#define bitsetclearcheck(byte1,n1bit,byte2,n2bit) (bitsetclear((byte1),(n1bit),bitcheck((byte2),(n2bit))))
 
 u32* encode(u32 *a, u32 size)
 {
     u32* b = new u32[size / 16]; // <- output tab
 
     for (u32 i = 0; i < size / 16; i++)
-    {   // Write size / 16 zeros to b
         b[i] = 0;
-    }
 
     for (u32 i = 0; i < size; i++)
         for (u32 j = 0; j < size; j++)
         {
             b[(i + j) / 32] ^= ( (a[i / 32] >> (i % 32)) & (a[j / 32 + size / 32] >> (j % 32)) & 1 ) << ((i + j) % 32);   // Magic centaurian operation
-            // if(i == 31 && j == 31)
-            //     cout << "i = " << setfill('0') << setw(2) << i << "; j = " << setfill('0') << setw(2) << j << "; b[" << dec << (i+j)/32 << "] = " << (std::bitset<32>(b[(i + j) / 32])) << ";" << endl;
+            cout << "i = " << setfill('0') << setw(2) << i << "; j = " << setfill('0') << setw(2) << j << "; b[" << dec << (i+j)/32 << "] = " << (std::bitset<32>(b[(i + j) / 32])) << ";" << endl;
         }
-    for(u32 i = 0; i < size / 16; i++)
-    {
-        if (i > 0)
-        {
-            s0 << ' ';
-        }
-        s0 << setfill('0') << setw(8) << hex << b[i];       // print result
-    }
-    s0 << endl;
-
-    /* 
-    Good luck humans     
-    */
     return b;
 }
 
 /**
  * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
+ * the standard input according to the problem statement.-------------------
  **/
+
+u32* decode(u32 *b, u32 size)
+{
+
+    u32* a = new u32[size / 16];
+
+    for(u32 i = 0; i < size / 16; ++i)
+    {
+        a[i] = 0;
+    }
+    // u32* b_prev = new u32[size / 16];
+    
+    // for (u32 i = 0; i < size / 16; i++)
+    //     b_prev[i] = b[i];
+
+    for (int i = size-1; i >= 0; i--)
+        for (int j = size-1; j >= 0; j--)
+        {
+            bitsetclearcheck(a[0],31,b[0],30);
+            bitsetclearcheck(a[size/32],31,b[size/32],30);
+            bitsetclearcheck(a[0],0,b[0],0);
+            bitsetclearcheck(a[size/32],0,b[0],0);
+            bitflip(b[size/32], 30);
+        }
+    //bitsetclear(a1[0],1,1);
+    //bitsetclear(a2[0],1,0);
+    return a;
+}
 
 int main()
 {
+    u32 count[16][32];
+    u32 size;
+    size = 32;
+    // size = 64;
+    // size = 128;
+    // size = 256;
+    cout << "SIZE = " << size << " :" << endl;
+    for(u32 i = 0; i < size / 16; ++i)
+        for (u32 j = 0; j < 32; j++)
+            count[i][j] = 0;
+    for (u32 i = 0; i < size; i++)
+        for (u32 j = 0; j < size; j++)
+            count[(i+j)/32][(i+j)%32]++;
+    
+    for (u32 i = 0; i < size / 16; i++)
+        for (u32 j = 0; j < 32; j++)
+            cout << "b[" << i << "][" << setfill('0') << setw(2) << j << "] = " << setfill('0') << setw(2) << count[i][j] << ";" << endl;
+    cout << endl;
+
     u32 s = 32;
     // cin >> s; cin.ignore();
-    string word1 = "46508fb7";
-    string word2 = "6677e201";
-    istringstream converter1(word1);
-    istringstream converter2(word2);
-    u32* b = new u32[2];
-    converter1 >> hex >> b[0];
-    converter2 >> hex >> b[1];
-
-    u32* a = decode(b, s);
+    istringstream convertera("46508fb7 6677e201");
+    u32* b = new u32[s / 16];
+    cout << "INPUT : " << endl;
     for(u32 i = 0; i < s / 16; i++)
     {
-        if (i > 0)
-        {
-            cout << ' ';
-        }
-        cout << setfill('0') << setw(8) << hex << a[i];       // print result
+        convertera >> hex >> b[i];
+        cout << setfill('0') << setw(8) << hex << b[i] << dec << " : " << (std::bitset<32>(b[i])) << ";" << endl;
     }
     cout << endl;
-    //cout << s0.str();
-    cout << "b 0 : " << (std::bitset<32>(a[0])) << ";" << endl;
-    cout << "b 1 : " << (std::bitset<32>(a[1])) << ";" << endl;
+
+    cout << "OUTPUT : " << endl;
+    u32* a = decode(b, s);
+    for(u32 i = 0; i < s / 16; i++)
+        cout << setfill('0') << setw(8) << hex << a[i] << dec << " : " << (std::bitset<32>(a[i])) << ";" << endl;
+    cout << endl;
+
+    istringstream converterc("b0c152f9 ebf2831f");
+    u32* c = new u32[2];
+    cout << "OUTPUT GOAL : " << endl;
+    for(u32 i = 0; i < s / 16; i++)
+    {
+        converterc >> hex >> c[i];
+        cout << setfill('0') << setw(8) << hex << c[i] << dec << " : " << (std::bitset<32>(c[i])) << ";" << endl;
+    }
+    encode(c, 32);
     // Write an answer using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
 
